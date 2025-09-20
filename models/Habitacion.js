735f4ae2ff00198135e4fca2f6ca22565215fdda
Vanruky <an.sevilla@duocuc.cl>
2025-09-20 20:52:51 -0300

    models v1

--- models/Habitacion.js
+++ models/Habitacion.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const habitacionSchema = new mongoose.Schema({
+  numero: { type: Number, required: true, unique: true },
+  tipo: { type: String, required: true },
+  precio: { type: Number, required: true },
+  disponible: { type: Boolean, default: true }
+});
+
+module.exports = mongoose.model('Habitacion', habitacionSchema);
