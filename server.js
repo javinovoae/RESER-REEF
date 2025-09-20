fc5fcae414545ac7378bb468400880ffa530b153
Vanruky <an.sevilla@duocuc.cl>
2025-09-20 20:49:15 -0300

    Server update ? v1.

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-ECHO est  activado.
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const habitacionRoutes = require('./routes/habitacionRoutes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/reserreef', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Conectado a MongoDB'))
+.catch(err => console.error(err));
+
+app.use('/api/habitaciones', habitacionRoutes);
+
+app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
